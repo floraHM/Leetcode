f38df6f0f6a00c4d67b6c9b1311341331a66fc52
floraHM <huangmei_6926@163.com>
2017-10-22 21:14:36 +0800

    第122题

---
+++ 122/122.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var total = 0;
+    for (var i=0; i< prices.length-1; i++) {
+        if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
+    }
+    return total;
+};
